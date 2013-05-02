ff323d768ace46ed1187eaf4cb96350b8183bf2a
masumi.murata <mathme.m@gmail.com>
2013-05-02 17:46:33 +0900

    githubのIP判定部分を修正

--- hook_base.php
+++ hook_base.php
@@ -1,17 +1,33 @@
 <?php
-//$githubIPs   = array("207.97.227.253", "50.57.128.197", "108.171.174.178", "50.57.231.61", "204.232.175.64", "192.30.252.0" );
-//$errorStatus = "HTTP/1.1 500 Internal Server Error";
-//if( !in_array( $_SERVER[ "REMOTE_ADDR" ], $githubIPs ) )
-//{
-//    header( $errorStatus );
-//    die;
-//}
 
-chdir("/var/www/html/github/reponame" );
-//shell_exec( "sudo -u gitwrite sh /var/www/html/github/reponame/sync.sh" );
+$githubIPs   = array("207.97.227.253/32", "50.57.128.197/32", "108.171.174.178/32", "50.57.231.61/32", "204.232.175.64/27", "192.30.252.0/22" );
+//アクセス元IPの判定処理
+foreach ($githubIPs as $ipAndMask){
+	$ipAndMask = chop($ipAndMask);
+	if (''	=== $ipAndMask) continue;
+	if ('#'	=== substr($ipAndMask, 0, 1)) continue;
 
-shell_exec( "sudo -u gitwrite git pull" );
-//shell_exec( "sudo -u gitwrite cp -rf /var/www/html/github/reponame /var/www/html/web/reponame" );
-//shell_exec( "sudo -u gitwrite rm -rf /var/www/html/web/reponame/.git /var/www/html/web/reponame/sync.sh" );
+	list($allowIp, $mask) = explode("/", $ipAndMask);
+
+	$remoteLong	= ip2long($_SERVER["REMOTE_ADDR"])>> (32 - $mask);
+	$allowLong	= ip2long($allowIp)>> (32 - $mask);
+
+	if ($remoteLong == $allowLong){
+		//githubからのcallと同時に渡されるJSONの有無を判定
+		$payload = json_decode( $_REQUEST[ "payload" ] );
+		if( $payload == NULL ){
+			header( $errorStatus );
+			die;
+		}
+		if( !chdir( "/var/www/html/github/" . $payload->repository->name ) ){
+			header( $errorStatus );
+			die;
+		}
+		shell_exec( "sudo -u mathme git pull" );
+		die;
+	}
+}
+
+// echo "許可されたIPからのアクセスではありません。";
 
 ?>
